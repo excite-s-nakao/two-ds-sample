544e1986366622fc00a445fede0cefbc419ef71e
Hiroki Ito <hiroki110@gmail.com>
2018-05-20 13:02:42 +0900

    add test case

---
+++ src/test/java/io/hrkt/twodssample/TwoDsSampleApplicationTest.java
@@ -0,0 +1,12 @@
+package io.hrkt.twodssample;
+
+import org.junit.Test;
+
+public class TwoDsSampleApplicationTest {
+
+  @Test
+  public void test() {
+    TwoDsSampleApplication.main(new String[0]);
+  }
+
+}
