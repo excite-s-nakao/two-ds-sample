c6fc234481a7cafd93cd8f095eeae7ca9d74a085
Hiroki Ito <hiroki110@gmail.com>
2018-05-23 05:41:57 +0900

    add print datasources

--- src/main/java/io/hrkt/twodssample/TwoDsSampleApplication.java
+++ src/main/java/io/hrkt/twodssample/TwoDsSampleApplication.java
@@ -1,12 +1,17 @@
 package io.hrkt.twodssample;
 
+import javax.sql.DataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.val;
 
 @SpringBootApplication
+@val
 public class TwoDsSampleApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(TwoDsSampleApplication.class, args);
+    val appCxt = SpringApplication.run(TwoDsSampleApplication.class, args);
+    val dsMap = appCxt.getBeansOfType(DataSource.class);
+    dsMap.entrySet().forEach(System.out::println);
   }
 }
